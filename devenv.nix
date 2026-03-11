e9fc5f9efd9287e215f3260d834c5f78851a73d6
shivangjhalani <shivang2004jhalani@gmail.com>
2026-03-11 18:43:07 +0530

    chore: update env setup

--- devenv.nix
+++ devenv.nix
@@ -1,10 +1,15 @@
-{ pkgs, lib, config, ... }:
+{ pkgs, lib, config, inputs, ... }:
 
 {
   dotenv.enable = true;
 
+  # https://devenv.sh/basics/
+  env.GREET = "LifeOS";
+
+  # https://devenv.sh/packages/
   packages = [ pkgs.zlib ];
 
+  # https://devenv.sh/languages/
   languages.python = {
     enable = true;
     uv = {
@@ -18,7 +23,37 @@
     pkgs.zlib
   ];
 
+  # https://devenv.sh/processes/
+  # processes.dev.exec = "${lib.getExe pkgs.watchexec} -n -- ls -la";
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/scripts/
+  scripts.hello.exec = ''
+    echo hello from $GREET
+  '';
+
+  # https://devenv.sh/basics/
   enterShell = ''
+    hello         # Run scripts directly
     echo "$(python --version | cut -d' ' -f2), uv $(uv --version | cut -d' ' -f2)"
   '';
+
+  # https://devenv.sh/tasks/
+  # tasks = {
+  #   "myproj:setup".exec = "mytool build";
+  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # };
+
+  # https://devenv.sh/tests/
+  # enterTest = ''
+  #   echo "Running tests"
+  #   git --version | grep --color=auto "${pkgs.git.version}"
+  # '';
+
+  # https://devenv.sh/git-hooks/
+  # git-hooks.hooks.shellcheck.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
 }
